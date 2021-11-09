6d35a0c4b2899e98fb2fb80fdf46b338b638ea5f
nahdluddin <93822283+NAHDLUDDIN@users.noreply.github.com>
2021-11-09 07:58:26 +0700

    Add files via upload

---
+++ Menghitung_luas_segitiga.py
@@ -0,0 +1,5 @@
+print ("Program Menghitung Luas Segitiga")
+a = float(input("Masukkan Alas = "))
+t = float(input("Masukkan Tinggi = "))
+luas = 0.5*a*t
+print ("Luas Segitiga adalah = " +str(luas))
